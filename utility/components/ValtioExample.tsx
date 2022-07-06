4cc4f3ea537bd7dd7c7a56c83567b44c6c886fec
Gerson Diniz <gersondinis@gmail.com>
2022-07-06 12:53:48 +0000

    Added MUI

---
+++ utility/components/ValtioExample.tsx
@@ -0,0 +1,40 @@
+import {Box, Card, CardContent, CardHeader, IconButton} from '@mui/material';
+import {Add, Remove} from '@mui/icons-material';
+import {proxy, useSnapshot} from 'valtio';
+
+export const state = proxy({
+  count: 0,
+});
+
+export const ValtioExample = () => {
+
+  const {count} = useSnapshot(state);
+
+  return (
+    <Card raised>
+      <CardHeader title={'valtio'} subheader={'state management'}/>
+      <CardContent>
+        <Box sx={styles.container}>
+          <IconButton onClick={() => --state.count}><Remove/></IconButton>
+          <Box sx={styles.counter}>
+            {count}
+          </Box>
+          <IconButton onClick={() => ++state.count}><Add/></IconButton>
+        </Box>
+      </CardContent>
+    </Card>
+  );
+};
+
+const styles = {
+  container: {
+    display: 'flex'
+  },
+  counter: {
+    display: 'flex',
+    fontSize: '40px',
+    padding: 1
+  }
+}
+
+export default ValtioExample;
